841decdb1b810ffb27ca317e75fe7c1df2d4d8f4
Dawntraoz <albasilventefu@gmail.com>
2025-04-07 19:56:40 +0200

    feat: add new web extension logo and corresponding icons

--- entrypoints/popup/App.vue
+++ entrypoints/popup/App.vue
@@ -1,11 +1,11 @@
 <script lang="ts" setup>
-import HelloWorld from '@/components/HelloWorld.vue';
+import HelloWorld from "@/components/HelloWorld.vue";
 </script>
 
 <template>
   <div>
     <a href="https://wxt.dev" target="_blank">
-      <img src="/wxt.svg" class="logo" alt="WXT logo" />
+      <img src="/prismai.svg" class="logo" alt="WXT logo" />
     </a>
     <a href="https://vuejs.org/" target="_blank">
       <img src="@/assets/vue.svg" class="logo vue" alt="Vue logo" />
