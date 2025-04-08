547e03bf81ebd20226240bacddb24980e01f0a04
Dawntraoz <albasilventefu@gmail.com>
2025-04-08 21:35:14 +0200

    fix: update .env variables to have WXT_ prefix and manifest config function syntax

--- wxt.config.ts
+++ wxt.config.ts
@@ -3,9 +3,10 @@ import { defineConfig } from 'wxt';
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   modules: ['@wxt-dev/module-vue'],
-  manifest: {
-    key: import.meta.env.WEB_EXTENSION_KEY,
+  // To use environment variables in the manifest, you need to use the function syntax: https://wxt.dev/guide/essentials/config/environment-variables.html#manifest
+  manifest: () => ({
+    key: import.meta.env.WXT_WEB_EXTENSION_KEY,
     permissions: ['aiLanguageModelOriginTrial'],
-    'trial_tokens': [import.meta.env.PROMPT_API_TOKEN]
-  }
+    'trial_tokens': [import.meta.env.WXT_PROMPT_API_TOKEN]
+  }),
 });
